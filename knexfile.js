44c0cffbbe932a9bae6325cf1240e3c3eb202c95
Jean Junior <jclgjunior@gmail.com>
2020-05-31 17:59:12 -0300

    Connect and config database

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'knex_test',
+      user: 'postgres',
+      password: 'postgres',
+      host: '127.0.0.17'
+    }
+  },
+
+};
